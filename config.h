cb97479a55a3fda9f5034066c7d8ec3811148569
Rumen <rumenmitov@protonmail.com>
2025-01-02 18:43:14 +0100

    config.h: colors

--- config.h
+++ config.h
@@ -1,26 +1,24 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *barlayout        = "tn|s";
-static const char *fonts[]          = { "JetBrains Mono:size=18" };
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
+static const unsigned int borderpx               = 1;        /* border pixel of windows */
+static const unsigned int snap                   = 32;       /* snap pixel */
+static const unsigned int gappih                 = 20;       /* horiz inner gap between windows */
+static const unsigned int gappiv                 = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh                 = 10;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov                 = 30;       /* vert outer gap between windows and screen edge */
+static       int smartgaps                       = 1;        /* 1 means no outer gap when there is only one window */
+static const int showbar                         = 1;        /* 0 means no bar */
+static const int topbar                          = 1;        /* 0 means bottom bar */
+static const char *barlayout                     = "tn|s";
+static const char *fonts[]                       = { "Hack Nerd Font:size=18", "JetBrains Mono:size=18" };
+static const char col_bg[]                       = "#27251f";
+static const char col_active[]                   = "#9bf6ff";
+static const char col_inactive[]                 = "#c4ced4";
+static const char *colors[][3]                   = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_inactive, col_bg, col_bg },
+	[SchemeSel]  = { col_active, col_bg, col_bg },
 };
 
 
